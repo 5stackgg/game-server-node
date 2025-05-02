9b7fa6ca163af44c57c19b5eb005bee4774580e1
Luke Policinski <luke@lukepolo.com>
2025-05-02 15:25:45 -0400

    bug: dont double upload, make sure to pass content size etc through

--- src/upload-demos.ts
+++ src/upload-demos.ts
@@ -1,10 +1,29 @@
 import fs from "fs";
 import path from "path";
+import { glob } from "glob";
 import fetch from "node-fetch";
 
 const DEMO_DIR = "/demos";
+const HASURA_GRAPHQL_ADMIN_SECRET = process.env
+  .HASURA_GRAPHQL_ADMIN_SECRET as string;
+
+let isUploading = false;
+
+type Demo = {
+  name: string;
+  size: number;
+  mapId: string;
+  matchId: string;
+  fullPath: string;
+};
 
 export async function uploadDemos() {
+  if (isUploading) {
+    return;
+  }
+
+  isUploading = true;
+
   const demos = await getDemos();
 
   if (!demos.length) {
@@ -13,126 +32,111 @@ export async function uploadDemos() {
 
   console.info(`found ${demos.length} demos`);
 
-  for (const { demo, mapId, matchId } of demos) {
+  for (const demo of demos) {
     try {
-      const fullPath = path.join(DEMO_DIR, matchId, mapId, demo);
-      const size = fs.statSync(fullPath).size;
+      console.info(`uploading demo`, demo);
 
-      console.info(`uploading demo`, {
-        matchId,
-        mapId,
-        size,
-        demo,
-      });
-
-      const response = await fetch(
-        `http://${process.env.API_SERVICE_HOST}:${process.env.API_SERVICE_PORT}/demos/${matchId}/pre-signed`,
+      const presignedResponse = await fetch(
+        `http://${process.env.API_SERVICE_HOST}:${process.env.API_SERVICE_PORT}/demos/${demo.matchId}/pre-signed`,
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
-            "hasura-admin-secret": process.env
-              .HASURA_GRAPHQL_ADMIN_SECRET as string,
+            "hasura-admin-secret": HASURA_GRAPHQL_ADMIN_SECRET,
           },
           body: JSON.stringify({
-            demo,
-            mapId,
+            demo: demo.name,
+            mapId: demo.mapId,
           }),
         },
       );
 
-      switch (response.status) {
+      switch (presignedResponse.status) {
         case 409:
           console.info(`match map is not finished`);
           break;
         case 406:
           console.info(`demo is already uploaded`);
-          fs.unlinkSync(fullPath);
+          fs.unlinkSync(demo.fullPath);
           break;
         case 410:
           console.info(`match map not found`);
-          fs.unlinkSync(fullPath);
+          fs.unlinkSync(demo.fullPath);
           break;
       }
 
-      if (!response.ok) {
-        console.error(`unable to get presigned url`, response.status);
+      if (!presignedResponse.ok) {
+        console.error(`unable to get presigned url`, presignedResponse.status);
         continue;
       }
 
-      const { presignedUrl } = (await response.json()) as {
+      const { presignedUrl } = (await presignedResponse.json()) as {
         presignedUrl: string;
       };
 
-      const success = await fetch(presignedUrl, {
+      const uploadResponse = await fetch(presignedUrl, {
         method: "PUT",
-        body: fs.createReadStream(fullPath),
+        body: fs.createReadStream(demo.fullPath),
         headers: {
+          "Content-Length": demo.size.toString(),
           "Content-Type": "application/octet-stream",
         },
       });
 
-      if (!success) {
-        console.error(`unable to upload demo`, matchId, mapId, demo);
+      if (!uploadResponse.ok) {
+        console.error(`unable to upload demo`, uploadResponse.status);
         continue;
       }
 
       await fetch(
-        `http://${process.env.API_SERVICE_HOST}:${process.env.API_SERVICE_PORT}/demos/${matchId}/uploaded`,
+        `http://${process.env.API_SERVICE_HOST}:${process.env.API_SERVICE_PORT}/demos/${demo.matchId}/uploaded`,
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
+            "hasura-admin-secret": HASURA_GRAPHQL_ADMIN_SECRET,
           },
           body: JSON.stringify({
-            demo,
-            mapId,
-            size,
+            demo: demo.name,
+            mapId: demo.mapId,
+            size: demo.size,
           }),
         },
       );
 
-      fs.unlinkSync(fullPath);
+      fs.unlinkSync(demo.fullPath);
+
+      console.info(`uploaded demo`, demo);
     } catch (error) {
       console.error(`unable to get presigned url`, error);
+    } finally {
+      const matchDir = path.join(DEMO_DIR, demo.matchId);
+      if (await checkIfPathEmpty(matchDir)) {
+        fs.rmdirSync(matchDir, { recursive: true });
+      }
     }
   }
 }
 
-async function getDemos() {
-  const availableDemos: Array<{
-    matchId: string;
-    mapId: string;
-    demo: string;
-  }> = [];
+async function checkIfPathEmpty(path: string) {
+  const files = await glob(`${path}/**/*.{dem}`, { dot: true });
+  return files.length === 0;
+}
+async function getDemos(): Promise<Array<Demo>> {
+  const availableDemos: Array<Demo> = [];
 
-  const demos = await fs.promises.readdir(DEMO_DIR, { withFileTypes: true });
+  const demoFiles = await glob(`${DEMO_DIR}/**/*.dem`, { dot: true });
 
-  for (const demo of demos) {
-    if (demo.isDirectory()) {
-      const matchId = demo.name;
-      const matchPath = path.join(DEMO_DIR, matchId);
-      const maps = await fs.promises.readdir(matchPath, {
-        withFileTypes: true,
-      });
+  for (const demoPath of demoFiles) {
+    const [matchId, mapId, name] = demoPath.split(path.sep).slice(-3);
 
-      for (const map of maps) {
-        if (map.isDirectory()) {
-          const mapPath = path.join(matchPath, map.name);
-          const mapDemos = await fs.promises.readdir(mapPath);
-
-          for (const demoFile of mapDemos) {
-            if (demoFile.endsWith(".dem")) {
-              availableDemos.push({
-                matchId,
-                mapId: map.name,
-                demo: demoFile,
-              });
-            }
-          }
-        }
-      }
-    }
+    availableDemos.push({
+      name,
+      mapId,
+      matchId,
+      fullPath: demoPath,
+      size: fs.statSync(demoPath).size,
+    });
   }
 
   return availableDemos;
