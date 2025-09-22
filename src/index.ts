3388c46143081c698ba23f5c8ba421b3dfdcaf42
Luke Policinski <Luke@LukePOLO.com>
2025-09-22 14:55:22 -0400

    chore: add health endpiont

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import http from "http";
 import { getPublicIP } from "./network";
 import { uploadDemos } from "./upload-demos";
 import { setupWebSocket, stopPing } from "./websocket";
@@ -5,6 +6,23 @@ import { setupWebSocket, stopPing } from "./websocket";
 const ipInterval = setInterval(async () => {
   await getPublicIP();
 }, 5 * 1000);
+
+const port = parseInt(process.env.HEALTH_PORT || "8080");
+
+const server = http.createServer((req, res) => {
+  if (req.method === "GET" && req.url && req.url.startsWith("/healthz")) {
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end();
+});
+
+server.listen(port, () => {
+  console.log(`http server listening on : ${port}`);
+});
 
 process.once("SIGUSR2", () => {
   stopPing();
